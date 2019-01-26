2da063c1467170ebfc16b499dadb06026b6287c4
Sagar-Rathi <sagarrathi099@gmail.com>
2019-01-26 19:58:57 +0500

    area of triangle

---
+++ main.js
@@ -0,0 +1,7 @@
+let base = prompt("Enter Base");
+let height = prompt("Enter Height");
+let area = 0.5*base*height;
+
+console.log("base is",+base);
+console.log("Height is",+height);
+console.log("Area is ",+area);
